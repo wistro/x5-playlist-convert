a0b7d038bf5f5e9d32488400e9b412f65190617a
jalestro <jalestro@users.noreply.github.com>
2015-11-13 18:57:37 -0600

    bash script update 2

--- playlist.sh
+++ playlist.sh
@@ -1,6 +1,6 @@
-#! /bin/bash
+#!/bin/bash
 
-while getopts aTp option
+while getopts a:Tp option
 do
   case "${option}"
     in
@@ -9,3 +9,19 @@ do
     p) PLAYLIST=${OPTARG};;
   esac
 done
+
+if [ -z $PLAYLIST ]
+then
+  PLAYLIST=$ARTIST
+fi
+
+PLAYLIST+=".m3u8"
+
+if [ -z $ALBUM ]
+then
+  list=`ls -lhR $ARTIST*`
+else
+  list=`ls -lhR $ARTIST*$ALBUM*`
+fi
+
+createPlaylist.pl $list $PLAYLIST
